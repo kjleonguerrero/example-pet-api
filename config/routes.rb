f80be6d6f89c36396b35ba06daee9a249ef0c600
kjleonguerrero <139664760+kjleonguerrero@users.noreply.github.com>
2023-11-15 07:32:58 +1000

    add index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
+  get "/pets" => "pets#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
-pet = Pet.create(name: "Bear", age: 2, breed: "German Shepherd")
-pet = Pet.create(name: "Blue", age: 1, breed: "German Shepherd")
+# pet = Pet.create(name: "Bear", age: 2, breed: "German Shepherd")
+# pet = Pet.create(name: "Blue", age: 1, breed: "German Shepherd")
